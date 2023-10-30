697f5de88dcc8446d25c0b2f0ccf3c169d581de1
yanralapdy <apdy@62teknologi.com>
2023-10-30 21:45:43 +0700

    first commit

---
+++ lib/Constant/const.dart
@@ -0,0 +1,5 @@
+//String url = "http://192.168.43.185";
+String url = "https://494f-120-188-38-241.ngrok-free.app/vigenesia";
+// String url = "http://192.168.1.9";
+
+// Change This For Different IP
